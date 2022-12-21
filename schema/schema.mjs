60957a6102f4a42a9603646726918fe1c4db4cdb
kizey <ikizey@icloud.com>
2022-12-21 18:39:00 +0400

    changed all resolvers to use mongodb.

--- schema/schema.mjs
+++ schema/schema.mjs
@@ -1,54 +1,5 @@
-const books = [
-  {
-    id: '1',
-    name: 'The Adventures of Tom Sawyer',
-    shortDescription: 'A classic tale of adventure and mischief',
-    authorIds: ['1'],
-  },
-  {
-    id: '2',
-    name: 'The Adventures of Huckleberry Finn',
-    shortDescription: 'A classic tale of friendship and adventure',
-    authorIds: ['1'],
-  },
-  {
-    id: '3',
-    name: "Harry Potter and the Philosopher's Stone",
-    shortDescription: 'A magical tale of adventure and friendship',
-    authorIds: ['2'],
-  },
-  {
-    id: '4',
-    name: 'Harry Potter and the Chamber of Secrets',
-    shortDescription: 'A magical tale of mystery and adventure',
-    authorIds: ['2'],
-  },
-  {
-    id: '5',
-    name: 'Harry Potter and the Prisoner of Azkaban',
-    shortDescription: 'The third book in the Harry Potter series.',
-    authorIds: ['2'],
-  },
-  {
-    id: '6',
-    name: 'The Prince and the Pauper',
-    shortDescription: 'A tale of mistaken identity.',
-    authorIds: ['1'],
-  },
-];
-
-const authors = [
-  {
-    id: '1',
-    name: 'Mark Twain',
-    bookIds: ['1', '2', '6'],
-  },
-  {
-    id: '2',
-    name: 'J.K. Rowling',
-    bookIds: ['3', '4', '5'],
-  },
-];
+import Book from '../models/book.mjs';
+import Author from '../models/author.mjs';
 
 export const typeDefs = `#graphql
   type Book {
@@ -81,103 +32,105 @@ export const typeDefs = `#graphql
 }
 `;
 
+const createDocument = async (Model, data) => {
+  try {
+    const newDocument = new Model(data);
+    const savedDocument = await newDocument.save();
+    return savedDocument;
+  } catch (error) {
+    console.error(error);
+    throw new Error(`Error creating ${Model.modelName}`);
+  }
+};
+
+const paginate = async (Model, { pageSize, page }) => {
+  if (pageSize && page) {
+    const startIndex = (page - 1) * pageSize;
+    return await Model.find().skip(startIndex).limit(pageSize);
+  }
+  return await Model.find();
+};
+
+const getDocumentById = async (Model, id) => {
+  return await Model.findOne({ id });
+};
+
+const updateDocument = async (Model, id, updatedFields) => {
+  try {
+    const updatedDocument = await Model.updateOne({ id }, updatedFields);
+    if (updatedDocument.nModified === 0) {
+      throw new Error(`Document with id ${id} not found`);
+    }
+    return updatedDocument;
+  } catch (error) {
+    throw new Error(error);
+  }
+};
+
+const deleteDocument = async (Model, id, fieldToUpdate, relatedModel) => {
+  try {
+    const deletedDocument = await Model.findByIdAndDelete(id);
+    if (!deletedDocument) {
+      throw new Error(`${Model.modelName} with id ${id} not found`);
+    }
+    await updateRelatedDocuments(
+      deletedDocument,
+      fieldToUpdate,
+      relatedModel,
+      Model
+    );
+    return true;
+  } catch (error) {
+    throw new Error(error);
+  }
+};
+
+const updateRelatedDocuments = async (
+  deletedDocument,
+  fieldToUpdate,
+  relatedModel,
+  Model
+) => {
+  deletedDocument[fieldToUpdate].forEach(async (relatedId) => {
+    const relatedDocument = await relatedModel.findById(relatedId);
+    const documentIndex = relatedDocument[fieldToUpdate].indexOf(
+      deletedDocument._id
+    );
+    relatedDocument[fieldToUpdate].splice(documentIndex, 1);
+    try {
+      await relatedDocument.save();
+    } catch (error) {
+      await Model.create(deletedDocument);
+      throw new Error(error);
+    }
+  });
+};
+
 export const resolvers = {
   Query: {
-    books: (root, { pageSize, page }) => {
-      if (pageSize && page) {
-        const startIndex = (page - 1) * pageSize;
-        return books.slice(startIndex, startIndex + pageSize);
-      }
-      return books;
-    },
-    book(root, { id }) {
-      return books.find((book) => book.id === id);
-    },
-    authors: (root, { pageSize, page }) => {
-      if (pageSize && page) {
-        const startIndex = (page - 1) * pageSize;
-        return authors.slice(startIndex, startIndex + pageSize);
-      }
-      return authors;
-    },
-    author(root, { id }) {
-      return authors.find((author) => author.id === id);
-    },
+    books: (root, args) => paginate(Book, args),
+    book: (root, { id, additionalArgument }) =>
+      getDocumentById(Book, id, additionalArgument),
+    authors: (root, args) => paginate(Author, args),
+    author: (root, { id, additionalArgument }) =>
+      getDocumentById(Author, id, additionalArgument),
   },
   Mutation: {
-    createBook: (root, args) => {
-      const newBook = {
-        id: String(Math.max(-1, ...books.map((book) => book.id)) + 1),
-        name: args.name,
-        shortDescription: args.shortDescription,
-        authorIds: args.authorIds,
-      };
-      books.push(newBook);
-      return newBook;
-    },
-    updateBook: (root, args) => {
-      const bookIndex = books.findIndex((book) => book.id === args.id);
-      if (bookIndex === -1) {
-        return {
-          id: '-1',
-          name: 'not found',
-          authorIds: [],
-        };
-      }
-      const updatedBook = {
-        ...books[bookIndex],
-        ...args,
-      };
-      books[bookIndex] = updatedBook;
-      return updatedBook;
-    },
-    deleteBook: (root, { id }) => {
-      const bookIndex = books.findIndex((book) => book.id === id);
-      if (bookIndex === -1) {
-        return false;
-      }
-      books.splice(bookIndex, 1);
-      return true;
-    },
-    createAuthor: (root, args) => {
-      const newAuthor = {
-        id: String(Math.max(-1, ...authors.map((book) => book.id)) + 1),
-        name: args.name,
-        bookIds: args.bookIds,
-      };
-      authors.push(newAuthor);
-      return newAuthor;
-    },
-    updateAuthor: (root, { id, name, bookIds }) => {
-      const index = authors.findIndex((author) => author.id === id);
-      if (index === -1) {
-        return {
-          id: '-1',
-          name: 'not found',
-          books: [],
-        };
-      }
-      const updatedAuthor = { ...authors[index], name, bookIds };
-      authors[index] = updatedAuthor;
-      return updatedAuthor;
-    },
-    deleteAuthor: (root, { id }) => {
-      const index = authors.findIndex((author) => author.id === id);
-      if (index === -1) {
-        return false;
-      }
-      authors.splice(index, 1);
-      return true;
-    },
+    createBook: (root, data) => createDocument(Book, data),
+    updateBook: (root, args) => updateDocument(Book, args.id, args),
+    deleteBook: (root, { id }) => deleteDocument(Book, id, 'authorIds', Author),
+    createAuthor: (root, data) => createDocument(Author, data),
+    updateAuthor: (root, args) => updateDocument(Author, args.id, args),
+    deleteAuthor: (root, { id }) => deleteDocument(Author, id, 'bookIds', Book),
   },
   Book: {
-    authors: ({ authorIds }) => {
-      return authors.filter((author) => authorIds.includes(author.id));
+    authors: async ({ authorIds }) => {
+      return await Author.find({ _id: { $in: authorIds } });
     },
   },
   Author: {
-    books: ({ bookIds }) => {
-      return books.filter((book) => bookIds.includes(book.id));
+    books: async ({ bookIds }) => {
+      return await Book.find({ _id: { $in: bookIds } });
     },
   },
 };
